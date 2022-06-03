6d1a90b9785423c647ca3c64ff8f5fba3a1e2926
MarceliG <marcel9817@gmail.com>
2022-06-03 16:38:07 +0200

    test_user

--- tests/test_ex1.py
+++ tests/test_ex1.py
@@ -1,11 +1,18 @@
-import pytest
+# import pytest
 
 
-@pytest.mark.slow
-def test_example():
-    print("test1")
-    assert 1 == 1
+# @pytest.fixture(scope="session")
+# def fixture_1():
+#     print("run-fixture_1")
+#     return 1
 
 
-def test_example1():
-    assert 1 == 1
+# def test_example():
+#     print("test1")
+#     assert 1 == 1
+
+
+# def test_example1(fixture_1):
+#     print("example_1")
+#     num = fixture_1
+#     assert num == 1
